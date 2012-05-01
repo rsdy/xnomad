14212d66bbbb387ed239ec8f78e34700a2fab9ab
rhapsodhy <me@rhapsodhy.hu>
2012-05-01 02:01:45 +0200

    vol.sh: use inotify instead of polling regularly

--- vol.sh
+++ vol.sh
@@ -1,1 +1,11 @@
-amixer get Master | awk -F'[][]' '/%/ {if ($6 == "off") { print "Muted" } else { print "Vol:", $2 }}' | head -n 1
+SOUND_DEV="/dev/snd/controlC0"
+
+volume() {
+	amixer get Master | awk -F'[][]' '/%/ {if ($6 == "off") { print "muted" } else { print "vol:", $2 }}' | head -n 1
+}
+
+while [ $? -eq 0 ] && [[ $(ps p $PPID | grep xmobar) ]];
+do
+	volume
+	inotifywait $SOUND_DEV -e ACCESS -e CLOSE_WRITE > /dev/null 2>/dev/null
+done
